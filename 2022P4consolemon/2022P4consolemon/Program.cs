0ee41b1c96d07496ecbaf8c9bbd7e6a74b113e30
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2022-05-09 10:53:19 +0200

    Opdracht Classes and Fuctions afgemaakt

--- 2022P4consolemon/2022P4consolemon/Program.cs
+++ 2022P4consolemon/2022P4consolemon/Program.cs
@@ -9,4 +9,31 @@ namespace _2022P4consolemon
             Console.WriteLine("Hello World!");
         }
     }
+    // health , energy , name , age
+    // damage , energycost , name , weapons
+    class ConsoleMon
+    {
+        internal int health = 100;
+        internal int energy = 5;
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+
+        internal void DepleteEnergy(int energy)
+        {
+            energy -= energy;
+        }
+    }
+
+    class Skill
+    {
+        internal int skillEnegry = 10;
+        internal int skillDamage = 10;
+        void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(skillEnegry);
+            target.TakeDamage(skillDamage);
+        }
+    }
 }
